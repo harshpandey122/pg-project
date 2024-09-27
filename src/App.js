331932378561f44d8343c9df53a6b490e160150e
unknown <77dharamveer77@gmai.com>
2024-09-27 15:08:30 +0530

    Dharamveer Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
-import Navbar from "./components/Navbar.js"
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Homepage from "./components/Homepage.js"
+import Services from "./components/Services.js"
 function App() {
   return (
-    <><Navbar/></>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import Services from './Services'
+import React from "react";
+import "./navbar.css"
+import { Box,Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
+    <Box
+      width={"100%"}
+      p="1rem 6%"
+      textAlign={"center"}
+      sx={{ boxShadow: 3, mb: 2 }}
+    >
     <header className="header">
-        <nav className='navbar'> 
-        <Link to="/services">Jai Shree Ram</Link>
-        </nav>
-
+      <nav className="navbar">
+      <Button variant="contained"><NavLink to="/services">Services</NavLink></Button>
+      <Button variant="contained"><NavLink to="/homepage">Home</NavLink></Button>
+      </nav>
     </header>
-  )
-}
+    </Box>
+  );
+};
 
-export default Navbar 
-
+export default Navbar;
